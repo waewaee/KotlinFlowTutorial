7338a63dbca4ec75e8ff84d22767d154e13c057a
htetarkarzaw <arkarzaw412@gmail.com>
2023-08-29 03:07:02 +0630

    stateFlow

--- app/src/main/java/com/waewaee/kotlinflowtutorial/MainActivity.kt
+++ app/src/main/java/com/waewaee/kotlinflowtutorial/MainActivity.kt
@@ -3,6 +3,7 @@ package com.waewaee.kotlinflowtutorial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
@@ -11,12 +12,33 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.waewaee.kotlinflowtutorial.ui.theme.KotlinFlowTutorialTheme
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
+
+    private val viewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+//        lifecycleScope.launch {
+//            repeatOnLifecycle(Lifecycle.State.STARTED) {
+//                viewModel.stateFlow.collectLatest { number ->
+//                    binding.tvCounter.text = number.toString()
+//                }
+//            }
+//        }
+
+        collectLatestLifecycleFlow(viewModel.stateFlow) { number ->
+            binding.tvCounter.text = number.toString()
+        }
+
         setContent {
             KotlinFlowTutorialTheme {
 
@@ -29,6 +51,14 @@ class MainActivity : ComponentActivity() {
                     }
                 }
             }
+        }
+    }
+}
+
+fun <T> ComponentActivity.collectLatestLifecycleFlow(flow: Flow<T>, collect: suspend (T) -> Unit) {
+    lifecycleScope.launch {
+        repeatOnLifecycle(Lifecycle.State.STARTED) {
+            flow.collectLatest(collect)
         }
     }
 }
