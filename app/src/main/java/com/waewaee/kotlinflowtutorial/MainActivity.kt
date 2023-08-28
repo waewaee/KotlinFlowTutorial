de896f20e3caf76fe25e6d7d8e0387f0ddfd8daa
htetarkarzaw <arkarzaw412@gmail.com>
2023-08-28 13:01:05 +0630

    with collectLatest

--- app/src/main/java/com/waewaee/kotlinflowtutorial/MainActivity.kt
+++ app/src/main/java/com/waewaee/kotlinflowtutorial/MainActivity.kt
@@ -5,14 +5,10 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.waewaee.kotlinflowtutorial.ui.theme.KotlinFlowTutorialTheme
@@ -22,16 +18,9 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             KotlinFlowTutorialTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
 
                 val viewModel = viewModel<MainViewModel>()
-                val time = viewModel.countDownValue.collectAsState(initial = 10)
+                val time = viewModel.countDownFlow.collectAsState(initial = 10)
                 Box(modifier = Modifier.fillMaxSize()) {
                     Text(
                         text = time.value.toString(),
@@ -41,21 +30,5 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    KotlinFlowTutorialTheme {
-        Greeting("Android")
     }
 }
--- app/src/main/java/com/waewaee/kotlinflowtutorial/MainViewModel.kt
+++ app/src/main/java/com/waewaee/kotlinflowtutorial/MainViewModel.kt
@@ -1,18 +1,35 @@
 package com.waewaee.kotlinflowtutorial
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.launch
 
 class MainViewModel: ViewModel() {
 
-    val countDownValue = flow<Int> {
+    val countDownFlow = flow<Int> {
         val startingValue = 10
         var currentValue = startingValue
         while (currentValue > 0) {
             delay(1000L)
             currentValue--
             emit(currentValue)
+        }
+    }
+
+    init {
+        collectFlow()
+    }
+
+    private fun collectFlow() {
+        viewModelScope.launch {
+            countDownFlow.collectLatest { time ->
+                delay(1500L)
+                println("The current time is $time.")
+            }
         }
     }
 }
